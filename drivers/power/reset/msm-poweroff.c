54babd90c48c4cae1cb3805157f3fdb842948c14
Sultan Alsawaf <sultan@kerneltoast.com>
2018-05-13 13:01:46 -0700

    power: reset: msm: Always perform a regular reboot upon panic
    Perform a normal reboot upon panic so the user won't find their phone
    rebooted into download mode or something.
    
    Also perform a regular reboot when the restart command is unknown.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -315,16 +315,15 @@ static void msm_restart_prepare(const char *cmd)
 				(cmd != NULL && cmd[0] != '\0'));
 	}
 
-#ifdef CONFIG_QCOM_PRESERVE_MEM
+	/* To preserve console-ramoops */
 	need_warm_reset = true;
-#endif
 
-	if (force_warm_reboot)
-		pr_info("Forcing a warm reset of the system\n");
-
-#ifdef CONFIG_MSM_PRESERVE_MEM
-	need_warm_reset = true;
-#endif
+	/* Perform a regular reboot upon panic or unspecified command */
+	if (in_panic || !cmd) {
+		__raw_writel(0x77665501, restart_reason);
+		cmd = NULL;
+		in_panic = false;
+	}
 
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (force_warm_reboot || need_warm_reset)
